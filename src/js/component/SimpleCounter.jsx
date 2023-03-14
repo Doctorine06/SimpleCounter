4e1069e0e05137b754c089ea6d845ef6f565768e
Doctorine06 <fergtheartist@gmail.com>
2023-03-14 02:37:45 +0000

    local

--- src/js/component/SimpleCounter.jsx
+++ src/js/component/SimpleCounter.jsx
@@ -4,13 +4,13 @@ function SimpleCounter(props){
     return(<div className="bigCounter"> 
      <div className="calendar"></div>
         <i className="far fa-clock"></i>
+    
+    
+     <div className="four"></div>
+     <div className="three"></div>
+     <div className="two"></div>
+    <div className="one"></div>
     </div>
-    
-    //  <div className="four"></div>
-    // <div className="three"></div>
-    // <div className="two"></div>
-    // <div className="one"></div>
-    
     )
 }
 
