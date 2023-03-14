8e32fccc49c7a5ca0bedf802992cc60c04e061a6
Doctorine06 <fergtheartist@gmail.com>
2023-03-14 02:48:52 +0000

    add css

--- src/js/component/SimpleCounter.jsx
+++ src/js/component/SimpleCounter.jsx
@@ -15,10 +15,11 @@ function SimpleCounter(props){
 }
 
  setInterval(function)(){
-    const four = Math.floor(counter)
-    const three = Math.floor(counter)
-    const two = Math.floor(counter)
-    const one = Math.floor(counter)
+    const four = Math.floor(counter/10000);
+    const three = Math.floor(counter/1000);
+    const two = Math.floor(counter/100);
+    const one = Math.floor(counter/10);
+    console.log(four,three,two,one);
 
     // ReactDOM.render(
         <SimpleCounter digitOne= {CountQueuingStrategy.indexOf()} />,
