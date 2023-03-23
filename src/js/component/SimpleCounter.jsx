22d906f5783e1f685565d93f4edbfa8f49848643
Doctorine06 <fergtheartist@gmail.com>
2023-03-23 00:56:43 +0000

    new

--- src/js/component/SimpleCounter.jsx
+++ src/js/component/SimpleCounter.jsx
@@ -1,5 +1,6 @@
 import {number} from "prop-types";
 import React from "react";
+import PropTypes from 'prop-types';
 
 function SimpleCounter(props){
     return(<div className="bigCounter"> 
@@ -18,29 +19,14 @@ function SimpleCounter(props){
 SimpleCounter.propTypes = {
     // You can declare that a prop is a specific JS primitive. By default, these
     // are all optional.
-    digitFour: ProTypes.number,
-    digitThree: ProTypes.number,
-    digitTwo: ProTypes.number,
-    digitOne: ProTypes.number
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
 };
 
-let counter = 0;
- setInterval(function(){
-    const four = Math.floor(counter/10000);
-    const three = Math.floor(counter/1000);
-    const two = Math.floor(counter/100);
-    const one = Math.floor(counter/10);
-    console.log(four,three,two,one);
 
-    counter++;
-}, 1000);
     // render your react
-ReactDOM.render(
-        <SimpleCounter digitOne= 
-        {CountQueuingStrategy.indexOf(digitOne)} />,
-        document.querySelector('#app')
-   );
-
 
 
 export default SimpleCounter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,4 +13,21 @@ const Home = () => {
 	);
 };
 
+let counter = 0;
+ setInterval(function(){
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter);
+    console.log(four,three,two,one);
+
+    counter++;
+}, 1000);
+
+ReactDOM.render(
+	<SimpleCounter digitOne= {one} digitTwo={two} digitThree={three} digitFour={four} />,
+	document.querySelector('#app')
+);
+
+
 export default Home;
