2fca286baab26a58810afb2e483179b6a5a0278d
Doctorine06 <fergtheartist@gmail.com>
2023-04-14 21:06:06 +0000

    message

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,66 +1,17 @@
-//import React from "react";
-import ReactDom from 'react-dom';
-//import SimpleCounter from './SimpleCounter.jsx';
-//import PropTypes from 'prop-types';
+import React from 'react'
+
+export default function Home(props) { 
+return(
+<div className="bigCounter">
+<div>{Math.floor(props.sec / 100000) % 10 }</div>
+<div>{Math.floor(props.sec / 10000) % 10 }</div>
+<div>{Math.floor(props.sec / 1000) % 10 }</div>
+<div>{Math.floor(props.sec / 100) % 10 }</div>
+<div>{Math.floor(props.sec / 10) % 10 }</div>
+<div>{Math.floor(props.sec / 1) % 10 }</div>
 
 
-//create your first component
-//const Home = () => {
-	//return (
-		<SimpleCounter />
-		//<PropTypes />
-		//<ReactDom />
-	//);
-//};
+</div>
 
-import {number} from "prop-types";
-import React from "react";
-import PropTypes from 'prop-types';
-
-function SimpleCounter(props){
-    return(<div className="bigCounter"> 
-     <div className="calendar"></div>
-        <i className="far fa-clock"></i>
-    
-    
-     <div className="four">{props.digitFour}</div>
-     <div className="three">{props.digitThree}</div>
-     <div className="two">{props.digitTwo}</div>
-    <div className="one">{props.digitOne}</div>
-    </div>
-    );
+  )
 }
-
-SimpleCounter.propTypes = {
-    // You can declare that a prop is a specific JS primitive. By default, these
-    // are all optional.
-    digitFour: PropTypes.number,
-    digitThree: PropTypes.number,
-    digitTwo: PropTypes.number,
-    digitOne: PropTypes.number
-};
-
-
-    // render your react
-
-
-export default SimpleCounter;)
-//(
-// let counter = 0;
-//  setInterval(function(){
-//     const four = Math.floor(counter/1000);
-//     const three = Math.floor(counter/100);
-//     const two = Math.floor(counter/10);
-//     const one = Math.floor(counter);
-//     console.log(four,three,two,one);
-
-//     counter++;
-// }, 1000);
-
-// ReactDOM.render(
-// 	<SimpleCounter digitOne= {one} digitTwo={two} digitThree={three} digitFour={four} />,
-// 	document.querySelector('#app')
-// );
-
-
-// export default Home;
