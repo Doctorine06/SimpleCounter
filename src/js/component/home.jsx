ec4f556bd44261fa469b175a7b76f9d1a35364de
Doctorine06 <fergtheartist@gmail.com>
2023-04-19 22:28:17 +0000

    pray for peace

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 export default function Home(props) { 
 return(
 <div className="bigCounter">
+<div className="calendar"></div>
+        <i className="far fa-clock"></i>
 <div>{Math.floor(props.sec / 100000) % 10 }</div>
 <div>{Math.floor(props.sec / 10000) % 10 }</div>
 <div>{Math.floor(props.sec / 1000) % 10 }</div>
